6b32b76f5739a582862170721c3e8429938a4fce
Olivier3456 <olivier.larrey@gmail.com>
2023-02-19 18:55:07 +0100

    Add fire sound for the indoor scene

---
+++ Assets/CreativeCore_Audio/Scripts/AudioTestScript.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTestScript : MonoBehaviour
+{
+ //   AudioSource audioSource;
+
+    public void TestAudio()
+    {
+    //    audioSource.PlayOneShot(audioSource.clip);
+        Debug.Log("Test OK");
+    }
+
+}
